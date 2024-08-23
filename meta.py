ee247f90ebfb3027ff674f313510a64ab2649fd8
Santhanalakshmi-220 <aestheticswathi@gmail.com>
2024-08-23 18:35:01 +0530

    Add files via upload

---
+++ meta.py
@@ -0,0 +1,33 @@
+HEADER_INFO = """""".strip()
+SIDEBAR_INFO = """
+<div class="contributors font-body text-bold">
+
+
+</p>
+</div>
+"""
+CHEF_INFO = """
+<h2 class="font-title">Welcome to our lovely restaurant! </h2>
+""".strip()
+PROMPT_BOX = "Add custom ingredients here (separated by `,`): "
+STORY = """
+
+""".strip()
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """.strip()
+hide_decoration_bar_style = """
+    <style>
+        header {visibility: hidden;}
+    </style>
+""".strip()
+hide_expander = """
+  <style>
+    .css-e370rw e19lei0e1{
+      visibility: hidden;
+    }
+  </style>
+"""
